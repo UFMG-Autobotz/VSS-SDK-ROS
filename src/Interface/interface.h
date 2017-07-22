50dfa328a1e566c7bc78801abd20b127b6f53b2a
Barbara Almeida <babialmeida1508@gmail.com>
2017-07-22 15:59:07 -0300

    edit interface.h para usar template

--- src/Interface/interface.h
+++ src/Interface/interface.h
@@ -1,110 +1,56 @@
 /*
- * This file is part of the VSS-SDK project.
- *
- * This Source Code Form is subject to the terms of the GNU GENERAL PUBLIC LICENSE,
- * v. 3.0. If a copy of the GPL was not distributed with this
- * file, You can obtain one at http://www.gnu.org/licenses/gpl-3.0/.
- */
+* This file is part of the VSS-SDK project.
+*
+* This Source Code Form is subject to the terms of the GNU GENERAL PUBLIC LICENSE,
+* v. 3.0. If a copy of the GPL was not distributed with this
+* file, You can obtain one at http://www.gnu.org/licenses/gpl-3.0/.
+*/
 
 #ifndef _INTERFACE_H_
 #define _INTERFACE_H_
+
+#include "ros/ros.h"
+#include <ros/callback_queue.h>
 
 #include <sstream>
 #include <iostream>
 #include <string>
 #include "unistd.h"
 
+//REVIEW confirir nomes das mensagens
+#include "vss_sdk/Global_State.h"
+#include "vss_sdk/Global_Commands.h"
+#include "vss_sdk/Global_Debug.h"
+
 using namespace std;
 
-//! Essa classe define as interfaces utilizadas em todos os projetos do VSS-SDK, em outras palavras, todos os sockets abertos em todos os projetos estão aqui.
+//! Essa classe define as interfaces utilizadas em todos os projetos do VSS-SDK
+template <class msg_Type>
 class Interface{
 protected:
-  ros::NodeHandle nh_state;
-  ros::NodeHandle nh_command_yellow;
-  ros::NodeHandle nh_command_blue;
-  ros::NodeHandle nh_debug;
+  ros::NodeHandle nh;
+  ros::CallbackQueue q;
+  ros::Publisher pub;
+  ros::Subscriber sub;
 
-  ros::CallbackQueue q_state;
-  ros::CallbackQueue q_command_yellow;
-  ros::CallbackQueue q_command_blue;
-  ros::CallbackQueue q_debug;
-
-  ros::Publisher pub_state;
-  ros::Publisher pub_command_yellow;
-  ros::Publisher pub_command_blue;
-  ros::Publisher pub_debug;
-
-  ros::Subscriber sub_state;
-  ros::Subscriber sub_command_yellow;
-  ros::Subscriber sub_command_blue;
-  ros::Subscriber sub_debug;
-
-	//! Pacote de estados (Utilizado pelo VSS-Simulator e VSS-Vision, para enviar informações do campo) (Utilizado também pelo VSS-Viewer, para desenhar a estado do jogo e pelo VSS-SampleStrategy para construir-se uma estratégia)
-	vss_sdk::Global_State *global_state;
-
-	//! Pacote de comandos (Utilizado por VSS-SampleStrategys e VSS-Joysticks, para enviar comandos para robôs virtuais) (Utilizado também pelo VSS-Simulator, para receber os comandos de cada robô)
- 	vss_sdk::Global_Commands *global_command_yellow;
-	vss_sdk::Global_Commands *global_command_blue;
-
-	//! Pacote de debug visual (Utilizado por VSS-SampleStrategys, para enviar informações da estratégia para o VSS-Viewer desenhar)
-	vss_sdk::Global_Debug *global_debug;
+  msg_Type *msg;
 
 public:
-	//! Construtor DEFAULT
-	Interface();
+  //! Construtor DEFAULT
+  Interface();
 
-	//! Método responsável por criar o socket de envio de estados no VSS-Vision e VSS-Simulator
-	void createSendState(vss_state::Global_State*, string addr_server_multicast = "tcp://*:5555");
-	//! Método respontável por enviar um novo estado pelo VSS-Vision e VSS-Simulator
-	void sendState();
-	//! Método responsável por criar o socket de recebimento de estados em VSS-SampleStrategys
-	void createReceiveState(vss_state::Global_State*, string addr_client_multicast = "tcp://localhost:5555");
-	//! Método responsável por receber um novo estado em VSS-SampleStrategys
-  void callbackReceiveState(const std_msgs::String::ConstPtr& msg);
-	void receiveState();
+  void createSend(msg_Type *message, std::string nodeName);
+  void send();
+  void createReceive(msg_Type *message, std::string nodeName);
+  void callback(const msg_Type message);
+  void receive();
 
-	//! Método responsável por criar o socket de envio de comandos em VSS-SampleStrategys ou VSS-Joysticks (Time Amarelo)
-	void createSendCommandsTeam1(vss_command::Global_Commands*, string addr_client_simulator_team1 = "tcp://localhost:5556");
-	//! Método responsável por enviar um novo comando por VSS-SampleStrategys ou VSS-Joysticks (Time Amarelo)
-	void sendCommandTeam1();
-	//! Método responsável por criar o socket de recebimento de comandos no VSS-Simulator (Time Amarelo)
-	void createReceiveCommandsTeam1(vss_command::Global_Commands*, string addr_server_simulator_team1 = "tcp://*:5556");
-	//! Método repsonsável por receber um novo comando (Time Amarelo)
-	void receiveCommandTeam1();
-
-	//! Método responsável por criar o socket de envio de comandos em VSS-SampleStrategys ou VSS-Joysticks (Time Azul)
-	void createSendCommandsTeam2(vss_command::Global_Commands*, string addr_client_simulator_team2 = "tcp://localhost:5557");
-	//! Método responsável por enviar um novo comando por VSS-SampleStrategys ou VSS-Joysticks (Time Azul)
-	void sendCommandTeam2();
-	//! Método responsável por criar o socket de recebimento de comandos no VSS-Simulator (Time Azul)
-	void createReceiveCommandsTeam2(vss_command::Global_Commands*, string addr_server_simulator_team2 = "tcp://*:5557");
-	//! Método repsonsável por receber um novo comando (Time Azul)
-	void receiveCommandTeam2();
-
-	//! Método responsável por criar o socket de envio de informações de debug em VSS-SampleStrategys ou VSS-Joysticks (Time Amarelo)
-	void createSendDebugTeam1(vss_debug::Global_Debug*, string addr_client_debug_team1 = "tcp://localhost:5558");
-	//! Método responsável por enviar uma nova informação de debug por VSS-SampleStrategys ou VSS-Joysticks (Time Amarelo)
-	void sendDebugTeam1();
-	//! Método responsável por criar o socket de recebimento de informações de debug no VSS-Simulator (Time Amarelo)
-	void createReceiveDebugTeam1(vss_debug::Global_Debug*, string addr_server_debug_team1 = "tcp://*:5558");
-	//! Método repsonsável por receber uma nova informação de debug (Time Amarelo)
-	void receiveDebugTeam1();
-
-	//! Método responsável por criar o socket de envio de informações de debug em VSS-SampleStrategys ou VSS-Joysticks (Time Azul)
-	void createSendDebugTeam2(vss_debug::Global_Debug*, string addr_client_debug_team2 = "tcp://localhost:5559");
-	//! Método responsável por enviar uma nova informação de debug por VSS-SampleStrategys ou VSS-Joysticks (Time Azul)
-	void sendDebugTeam2();
-	//! Método responsável por criar o socket de recebimento de informações de debug no VSS-Simulator (Time Azul)
-	void createReceiveDebugTeam2(vss_debug::Global_Debug*, string addr_server_debug_team2 = "tcp://*:5559");
-	//! Método repsonsável por receber uma nova informação de debug (Time Azul)
-	void receiveDebugTeam2();
-
-	//! Método que pode ser utilizado para imprimir o pacote de estado recebido/enviado no terminal
-	void printState();
-	//! Método que pode ser utilizado para imprimir o pacote de comando recebido/enviado no terminal
-	void printCommand();
-	//! Método que pode ser utilizado para imprimir o pacote de debug recebido/enviado no terminal
-	void printDebug();
+  //! Método que pode ser utilizado para imprimir o pacote de estado recebido/enviado no terminal
+  void printState();
+  //! Método que pode ser utilizado para imprimir o pacote de comando recebido/enviado no terminal
+  void printCommand();
+  //! Método que pode ser utilizado para imprimir o pacote de debug recebido/enviado no terminal
+  void printDebug();
 };
 
 #endif // _INTERFACE_H_
