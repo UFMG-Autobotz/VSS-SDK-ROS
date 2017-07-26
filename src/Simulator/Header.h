c0ee550e131beeb1236fb8c4c4aed4b520eac246
Barbara Almeida <babialmeida1508@gmail.com>
2017-07-26 19:38:37 -0300

    continuacao compilacao simulator

--- src/Simulator/Header.h
+++ src/Simulator/Header.h
@@ -18,8 +18,12 @@ copies or substantial portions of the Software.
 
 //Classes from lib
 
-#include "Constants.h"
-#include "btBulletDynamicsCommon.h"
+ #include "utils/Constants.h"
+#include <bullet/LinearMath/btVector3.h>
+//#include "LinearMath/btStraight.h"
+//#include <bullet/LinearMath/btStraight.h>
+#include "bullet/LinearMath/btStraight.h"
+#include <btBulletDynamicsCommon.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
--- src/Simulator/strategies/ModelStrategy.h
+++ src/Simulator/strategies/ModelStrategy.h
@@ -16,55 +16,55 @@ copies or substantial portions of the Software.
 #ifndef ModelStrategy_H_
 #define ModelStrategy_H_
 
-#include "Header.h"
-#include "RobotStrategy.h"
+#include "../Header.h"
+#include "../RobotStrategy.h"
 
 #define TURN_ROBOT 2
 #define SIMULATOR_TO_VSS 1
 
 class ModelStrategy{
 protected:
-    clock_t begin_time;
-    float deltaTime;
-    int numFramesPerSec;
+  clock_t begin_time;
+  float deltaTime;
+  int numFramesPerSec;
 
-    int attackDir;
+  int attackDir;
 
-	btVector3 ballPos;
-	btVector3 ballVel;
-	btVector3 ballAce;
-	btVector3 posGoalpost;
-	float timeStep;
+  btVector3 ballPos;
+  btVector3 ballVel;
+  btVector3 ballAce;
+  btVector3 posGoalpost;
+  float timeStep;
 
-    vector<RobotStrategy*> robotStrategiesTeam;
-	vector<RobotStrategy*> robotStrategiesAdv;
-	vector<ModelStrategy*> strategyHistory;
+  vector<RobotStrategy*> robotStrategiesTeam;
+  vector<RobotStrategy*> robotStrategiesAdv;
+  vector<ModelStrategy*> strategyHistory;
 
-	virtual void updateStrategiesHistory();
+  virtual void updateStrategiesHistory();
 
-    virtual void updateTargetPosition(RobotStrategy* robotStrategy);
+  virtual void updateTargetPosition(RobotStrategy* robotStrategy);
 
-    virtual void updateDynamics();
-    virtual void updateLocalDynamics(RobotStrategy* cur, RobotStrategy* old);
+  virtual void updateDynamics();
+  virtual void updateLocalDynamics(RobotStrategy* cur, RobotStrategy* old);
 
 public:
-    ModelStrategy();
-    void runStrategy();
-	virtual void runStrategy(vector<RobotStrategy*> robotStrategiesTeam, vector<RobotStrategy*> robotStrategiesAdv, btVector3 ballPos) = 0;
-	virtual void setAttackDir(int goalpostDir);
-	int getAttackDir(){ return attackDir; }
-    virtual void setFramesSec(int frames);
+  ModelStrategy();
+  void runStrategy();
+  virtual void runStrategy(vector<RobotStrategy*> robotStrategiesTeam, vector<RobotStrategy*> robotStrategiesAdv, btVector3 ballPos) = 0;
+  virtual void setAttackDir(int goalpostDir);
+  int getAttackDir(){ return attackDir; }
+  virtual void setFramesSec(int frames);
 
-	virtual vector<ModelStrategy*> getStrategyHistory(){ return strategyHistory; }
+  virtual vector<ModelStrategy*> getStrategyHistory(){ return strategyHistory; }
 
-	virtual vector<RobotStrategy*> getRobotStrategiesTeam(){ return robotStrategiesTeam; }
-	virtual vector<RobotStrategy*> getRobotStrategiesAdv(){ return robotStrategiesAdv; }
+  virtual vector<RobotStrategy*> getRobotStrategiesTeam(){ return robotStrategiesTeam; }
+  virtual vector<RobotStrategy*> getRobotStrategiesAdv(){ return robotStrategiesAdv; }
 
-	btVector3 getBallPosition(){ return ballPos; }
-	btVector3 getBallVelocity(){ return ballVel; }
-	btVector3 getBallAcceleration(){ return ballAce; }
+  btVector3 getBallPosition(){ return ballPos; }
+  btVector3 getBallVelocity(){ return ballVel; }
+  btVector3 getBallAcceleration(){ return ballAce; }
 
-	float getAttackGoal(){ return attackDir; }
+  float getAttackGoal(){ return attackDir; }
 };
 
 #endif
